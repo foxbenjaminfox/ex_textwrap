4890692bd7062587e393c1aa86e79c410700a4a1
Benjamin Fox <benjaminfox@hey.com>
2021-07-08 18:39:55 +0300

    Release v0.1.0

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ by adding `textwrap` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:textwrap, "~> 0.1.0-rc.0"}
+    {:textwrap, "~> 0.1.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Textwrap.MixProject do
   def project do
     [
       app: :textwrap,
-      version: "0.1.0-rc.0",
+      version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
